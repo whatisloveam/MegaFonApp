a300e2a89001459efcd15a4f0e77b0aeafb178f0
Dmitry Shchapin <diman199806@gmail.com>
2018-11-16 23:12:32 +0500

    http client with retries

---
+++ InvalidAppHttpClient/HelperExtensions.cs
@@ -0,0 +1,14 @@
+using System.Linq;
+using System.Net.Http;
+
+namespace InvalidAppHttpClient
+{
+    internal static class Helper
+    {
+        public static string GetHeader(this HttpResponseMessage message, string name) => message.Content.Headers.GetValues(name).Single();
+
+        public static Result<T> Deserialize<T>(this Result<string> result) => result.Select(JsonConvert.DeserializeObject<T>);
+
+        public static Result<T> ToResult<T>(this T value) => Result<T>.Success(value);
+    }
+}
