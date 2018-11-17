5944ddf03c4b32a5a1711360408ecb42ccdacc3c
Администратор <@->
2018-11-17 13:41:01 +0500

    TestAAAAA

---
+++ MegafonInvalidApp/Choose.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MegafonInvalidApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class Choose : Window
+    {
+        public Choose()
+        {
+            InitializeComponent();
+            
+        }
+        void OnClick1(object sender, RoutedEventArgs e)
+        {
+            //btn1.Background = Brushes.Pink;
+        }
+        void OnClick2(object sender, RoutedEventArgs e)
+        {
+            //btn2.Background = Brushes.Pink;
+        }
+    }
+}
